20bf9fce9d16bfcc91872743a23b8e61d4fd4754
Jackie-Pan <Jackie.Pan@studentfr.ch>
2026-02-05 09:25:55 +0100

    modification

--- src/frontend/app.js
+++ src/frontend/app.js
@@ -1,5 +1,5 @@
 //const apiEndpoint = "https://fa-todo-backend-baqmes.azurewebsites.net/api/tasks";
-const apiEndpoint = "https://baqtestappbackend.blackglacier-c7b0844e.northeurope.azurecontainerapps.io/api/tasks";
+const apiEndpoint = "https://ca-fd22-210-pan-backend.proudglacier-2a4adfe7.northeurope.azurecontainerapps.io/api/tasks";
 
 $(document).ready(function () {
   // Charger les tâches au démarrage
